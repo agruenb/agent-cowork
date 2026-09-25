ecc97432a2494433e552283ef068d34db99b4e2c
Adrian Gruenberg <adriang2000@gmx.de>
2026-09-25 19:08:04 +0200

    fix: update markdown editor layout padding

--- src/markdownEditorProvider.ts
+++ src/markdownEditorProvider.ts
@@ -246,7 +246,7 @@ export class MarkdownEditorProvider implements vscode.CustomTextEditorProvider {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} https: data:; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}'; font-src ${webview.cspSource};">
-  <link rel="stylesheet" href="${styleUri}">
+  <link rel="stylesheet" href="${styleUri}?v=${Date.now()}">
   <title>Markdown Editor</title>
 </head>
 <body>
